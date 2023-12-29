61c78959df6792614e7dd28c79b5333ed49b13ef
Dũng Bùi <dungpro96bn@gmail.com>
2023-12-29 16:52:09 +0700

    css 404 page

--- wp-content/themes/tagger-travel/404.php
+++ wp-content/themes/tagger-travel/404.php
@@ -1,10 +1,12 @@
 <?php get_header(); ?>
 
-<div id="404" class="404">
-  <div class="not-found-box">
-    <p class="not-found-text">申し訳ございません。<br>お探しのページは見つかりませんでした。</p>
-    <a href="<?php echo do_shortcode('[homePath]'); ?>" class="not-found-button">トップページへ戻る</a>
-  </div>
+<div id="page-not-found" class="404">
+    <div class="inner">
+        <div class="not-found-box dnp-s-m">
+            <p class="not-found-text">申し訳ございません。<br>お探しのページは見つかりませんでした。</p>
+            <a href="<?php echo do_shortcode('[homePath]'); ?>" class="not-found-button">トップページへ戻る</a>
+        </div>
+    </div>
 </div><!-- #404 -->
 
 <?php get_footer(); ?>
