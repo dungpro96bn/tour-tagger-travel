3561e8230ed9278ee66b730e396baa577a5001f7
Dũng Bùi <dungpro96bn@gmail.com>
2024-01-08 11:37:38 +0700

    style single page

--- wp-content/themes/tagger-travel/single.php
+++ wp-content/themes/tagger-travel/single.php
@@ -1,21 +1,20 @@
 <?php get_header(); ?>
 
     <div id="new" class="new-container">
-        <p class="new-heading">
-            <span class="letter-top">NEW</span>
-            <span class="letter-bottom">お知らせ</span>
-        </p>
-        <?php
-        if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <article id="post-<?php the_ID(); ?>" class="new-post">
-                <div class="single-body">
-                    <?php the_content(); ?>
-                </div>
-            </article><!-- .new-post -->
-        <?php endwhile; ?>
-        <?php else : ?>
-            <p class="not-post">このカテゴリーにはまだ記事がありません。</p>
-        <?php endif; ?>
+        <div class="inner">
+            <?php
+            if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <article id="post-<?php the_ID(); ?>" class="new-post">
+                    <div class="single-body">
+                        <?php the_content(); ?>
+                    </div>
+                </article><!-- .new-post -->
+            <?php endwhile; ?>
+            <?php else : ?>
+                <p class="not-post">このカテゴリーにはまだ記事がありません。</p>
+            <?php endif; ?>
+        </div>
+
     </div><!-- #new -->
 
 <?php get_footer(); ?>
